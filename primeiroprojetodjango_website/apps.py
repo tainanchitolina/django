e3d9e6ec4d32c376c39e0995b90a7042feac8614
tainanchitolina <chitolinatainan@hotmail.com>
2022-02-22 09:34:06 -0300

    Primeira parte do projeto django

---
+++ primeiroprojetodjango_website/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PrimeiroprojetodjangoWebsiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'primeiroprojetodjango_website'
