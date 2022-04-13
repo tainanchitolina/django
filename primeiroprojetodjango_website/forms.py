a233f64a43fa119e950c4dda4446ccab8bbfe2dc
Tainan Chitolina <89881708+tainanchitolina@users.noreply.github.com>
2022-04-13 20:58:27 -0300

    Delete primeiroprojetodjango_website directory

--- primeiroprojetodjango_website/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms 
-from primeiroprojetodjango_website.models import Pergunta
-
-class PerguntaForm(forms.ModelForm):
-  class Meta:
-    fields = ['enunciado']
-    model = Pergunta 
